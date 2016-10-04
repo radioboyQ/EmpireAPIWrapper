0151f24c46403405872f0efcd313814f828e263c
Scott Fraser <scott.fraser@guidepointsecurity.com>
2016-10-03 23:24:25 -0400

    More test harness updates

--- tests/main_test.py
+++ tests/main_test.py
@@ -4,9 +4,15 @@ api = EmpireAPIWrapper.empireAPI('10.15.20.157', uname='empireadmin', passwd='Pa
 # api = EmpireAPIWrapper.empireAPI('10.15.20.157', token='2zqb4bgvoq1jhe9essncl3qa6h9rvbj1jq2p740k')
 # api = EmpireAPIWrapper.empireAPI('10.15.20.157', token='yv42s1wlo90ikrzc7pwebgrbpqnzkigqlxbb4cp2')
 
+
+#### Admin and Utilties
 # print(api.check_version())
 # print(api.shutdownServer())
 # print(api.restartServer())
 # print(api.getMap())
 # print(api.getConfig())
 # print(api.getPermToken())
+
+##### Reporting
+# print(api.report())
+# print(api.report_agent('UG3YT4FNPRSKRZHA'))

b9660a56f15f3097c4dc0ad394021f49a443579c
Scott Fraser <scott.fraser@guidepointsecurity.com>
2016-10-03 22:20:26 -0400

    Updated how the HTTP errors are handled.

--- EmpireAPIWrapper/exceptions.py
+++ EmpireAPIWrapper/exceptions.py
@@ -2,3 +2,17 @@
 
 class NoAuthenticationProvided(Exception):
     """No authentication was provided when instantiating the module."""
+class InvalidToken(Exception):
+    """Token is not valid."""
+
+class HTTPError(Exception):
+    """Generic HTTP Error"""
+    class BadRequest(Exception):
+        """The request is not valid; HTTP 400"""
+    class UnAuthorized(Exception):
+        """Unauthorized; HTTP 401"""
+    class MethodNotAllowed(Exception):
+        """Wrong HTTP method used; HTTP 405"""
+    class UnKnownHTTPError(Exception):
+        """HTTP Error catch all"""
+

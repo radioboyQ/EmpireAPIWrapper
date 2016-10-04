ffb93d2a33d847d5aade8f69db87991b17698613
Scott Fraser <scott.fraser@guidepointsecurity.com>
2016-10-03 23:00:28 -0400

    Test harness for all working end points

---
+++ tests/main_test.py
@@ -0,0 +1,12 @@
+import EmpireAPIWrapper
+
+api = EmpireAPIWrapper.empireAPI('10.15.20.157', uname='empireadmin', passwd='Password123!')
+# api = EmpireAPIWrapper.empireAPI('10.15.20.157', token='2zqb4bgvoq1jhe9essncl3qa6h9rvbj1jq2p740k')
+# api = EmpireAPIWrapper.empireAPI('10.15.20.157', token='yv42s1wlo90ikrzc7pwebgrbpqnzkigqlxbb4cp2')
+
+# print(api.check_version())
+# print(api.shutdownServer())
+# print(api.restartServer())
+# print(api.getMap())
+# print(api.getConfig())
+# print(api.getPermToken())

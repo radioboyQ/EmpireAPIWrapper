2f150910aacbabe8936225642a0dcc35d3285123
Scott Fraser <scott.fraser@guidepointsecurity.com>
2016-10-05 20:09:09 -0400

    More test harness.

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,6 @@
 import EmpireAPIWrapper
 
-api = EmpireAPIWrapper.empireAPI('10.15.20.157', uname='empireadmin', passwd='Password123!')
+api = EmpireAPIWrapper.empireAPI('172.16.242.191', uname='empireadmin', passwd='Password123!')
 # api = EmpireAPIWrapper.empireAPI('10.15.20.157', token='2zqb4bgvoq1jhe9essncl3qa6h9rvbj1jq2p740k')
 # api = EmpireAPIWrapper.empireAPI('10.15.20.157', token='yv42s1wlo90ikrzc7pwebgrbpqnzkigqlxbb4cp2')
 
@@ -44,5 +44,4 @@ api = EmpireAPIWrapper.empireAPI('10.15.20.157', uname='empireadmin', passwd='Pa
 # data = {'Agent': agent_name, 'Listener': 'victor'}
 # print(api.module_exec('privesc/bypassuac', options=data))
 
-
-print(api.agent_run_shell_cmd('ZKRGK3Y2E2ZHTECE', {'command':'mimikatz'}))
+print(api.module_by_name('credentials/mimikatz/certs'))
